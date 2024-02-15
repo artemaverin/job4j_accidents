7036db59dd7c6215753b5fc7ba9909d51e2f46e4
Artem Averin <artem-averin-91@mail.ru>
2024-02-15 19:18:32 +0300

    1. Загрузка данных в вид [#295782]

--- src/main/java/ru/job4j/accidents/controller/IndexController.java
+++ src/main/java/ru/job4j/accidents/controller/IndexController.java
@@ -1,12 +1,15 @@
 package ru.job4j.accidents.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
     @GetMapping({"/", "/index"})
-    public String getIndex() {
+    public String getIndex(Model model) {
+        model.addAttribute("user", "Petr Arsentev");
+        model.addAttribute("class_active", "index");
         return "index";
     }
 }
